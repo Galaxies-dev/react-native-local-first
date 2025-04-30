bece32eb091602a95b3e41e1d89be57d560063de
Simon Grimm <saimon24@users.noreply.github.com>
2025-04-30 14:08:59 +0200

    Update SupabaseConnector.ts

--- powersync/SupabaseConnector.ts
+++ powersync/SupabaseConnector.ts
@@ -60,7 +60,7 @@ export class SupabaseConnector implements PowerSyncBackendConnector {
     console.debug('session expires at', session.expires_at);
 
     return {
-      cliennt: this.client,
+      client: this.client,
       endpoint: powersyncUrl,
       token: session.access_token ?? '',
       expiresAt: session.expires_at ? new Date(session.expires_at * 1000) : undefined,
